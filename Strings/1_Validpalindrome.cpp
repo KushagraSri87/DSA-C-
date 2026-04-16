38524e77830dcc9ae7d6eff46db84dd02f2a67a1
Kushagra Srivastava <srivastavakushagra2005@gmail.com>
2026-04-16 20:41:48 +0530

    Changed the code

--- Strings/1_Validpalindrome.cpp
+++ Strings/1_Validpalindrome.cpp
@@ -1,60 +1,65 @@
-#include<string>
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <string>
 using namespace std;
-class Solution {
-private:
-    bool valid(char ch) {
-        if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
-            return 1;
-        }
-        
-        return 0;
-    }
-    
-    char toLowerCase(char ch) {
-    if( (ch >='a' && ch <='z') || (ch >='0' && ch <='9') )
-        return ch;
-    else{
-        char temp = ch - 'A' + 'a';
-        return temp;
-    }
-}
-    bool checkPalindrome(string a) {
-    int s = 0;
-    int e = a.length()-1;
 
-    while(s<=e) {
-        if(a[s] != a[e])
-        {
-            return 0;       
-        }
-        else{
-            s++;
-            e--;
-        }
+bool valid(char ch) {
+    if ((ch >= 'a' && ch <= 'z') ||
+        (ch >= 'A' && ch <= 'Z') ||
+        (ch >= '0' && ch <= '9')) {
+        return true;
     }
-    return 1;
+    return false;
 }
-public:
-    bool isPalindrome(string s) {
-        
-        //faltu character hatado
-        string temp = "";
-        
-        for(int j=0; j<s.length(); j++) {   
-            if(valid(s[j])) {
-                temp.push_back(s[j]);
-            }
+
+char toLowerCase(char ch) {
+    if ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9'))
+        return ch;
+    else
+        return ch - 'A' + 'a';
+}
+
+bool checkPalindrome(string a) {
+    int s = 0;
+    int e = a.length() - 1;
+
+    while (s <= e) {
+        if (a[s] != a[e]) {
+            return false;
         }
-        
-        //lowercase me kardo
-        for(int j=0; j<temp.length(); j++) { 
-            temp[j] = toLowerCase(temp[j]);
-        }
-        
-        //check palindrome
-        return checkPalindrome(temp);
-        
+        s++;
+        e--;
     }
-};
+    return true;
+}
+
+bool isPalindrome(string s) {
+    string temp = "";
+
+    // Remove invalid characters
+    for (int j = 0; j < s.length(); j++) {
+        if (valid(s[j])) {
+            temp.push_back(s[j]);
+        }
+    }
+
+    // Convert to lowercase
+    for (int j = 0; j < temp.length(); j++) {
+        temp[j] = toLowerCase(temp[j]);
+    }
+
+    return checkPalindrome(temp);
+}
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    if (isPalindrome(input)) {
+        cout << "Palindrome ✅" << endl;
+    } else {
+        cout << "Not a Palindrome ❌" << endl;
+    }
+
+    return 0;
+}
